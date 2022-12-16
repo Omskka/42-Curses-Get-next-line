e99754a70b89350063bddc7f5de3ef4b6dd3af78
Omskka <92455486+Omskka@users.noreply.github.com>
2022-12-16 12:20:19 +0300

    Add files via upload

---
+++ Get_next_line/get_next_utils.c
@@ -0,0 +1,138 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_utils.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: okirca <okirca@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/15 16:48:28 by okirca            #+#    #+#             */
+/*   Updated: 2022/12/15 18:23:25 by okirca           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return ((char *)s + i);
+		i++;
+	}
+	if (s[i] == c)
+		return ((char *)s + i);
+	return (NULL);
+}
+
+char	*ft_strjoin(char *s1, char const *s2)
+{
+	char	*str;
+	int		i;
+	int		j;
+
+	i = -1;
+	j = 0;
+	if (!s1)
+	{
+		s1 = malloc(1 * sizeof(char));
+		*s1 = '\0';
+	}
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!str)
+		return (NULL);
+	while (s1[++i] != '\0')
+		str[i] = s1[i];
+	while (s2[j] != '\0')
+	{
+		str[i] = s2[j];
+		i++;
+		j++;
+	}
+	str[i] = '\0';
+	free(s1);
+	return (str);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_substr(const char *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	i;
+	size_t	strsayac;
+
+	i = -1;
+	strsayac = -1;
+	if (!s)
+		return (0);
+	if (start >= ft_strlen((char *)s))
+		return (ft_strdup(""));
+	if (len > ft_strlen(s))
+		str = malloc(sizeof(char) * ft_strlen(s) + 1);
+	else
+		str = malloc(sizeof(char) * len + 1);
+	if (!str)
+		return (0);
+	while (s[start] && ++i <= len)
+	{
+		str[++strsayac] = (char)s[start];
+		start++;
+	}
+	str[len] = '\0';
+	return (str);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*str;
+	size_t	i;
+
+	if (!s1)
+		return (NULL);
+	str = (char*)malloc(sizeof(*s1) * (ft_strlen(s1) + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	str[i] = 0;
+	return (str);
+}
+
+char	*ft_find(char *buffer)
+{
+	char	*buff;
+	int		i;
+
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	if (!buffer[i])
+	{
+		free(buffer);
+		return (NULL);
+	}
+	i++;
+	buff = ft_substr(buffer, i, (ft_strlen(buffer) - i));
+	if (!buff)
+		return (NULL);
+	free(buffer);
+	return (buff);
+}
